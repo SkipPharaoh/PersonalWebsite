9f5e22273a8ec962d86b5d611c0eb6c763d6214f
Sonya Lawrence-Thompson <92489108+Sonya-7@users.noreply.github.com>
2024-09-03 15:34:47 -0400

    Update SonyaLawrenceThompsonResume.py

--- SonyaLawrenceThompsonResume.py
+++ SonyaLawrenceThompsonResume.py
@@ -5,8 +5,8 @@ Header = '   This resume was generated entirely in Python using matplotlib.\n   
 Name = 'SONYA LAWRENCE-THOMPSON'.upper()
 Title = 'Financial Data Analysis & Data Science'
 contactHeader = 'CONTACT'
-Contact = 'Jacksonville, FL\n904-615-5819\nlawrences@huntington.edu\nlinkedin.com/in/sonya-lt\nhttps://sonya-personal-website.vercel.app'
-Portfolio = 'https://sonya-personal-website.vercel.app'
+Contact = 'Jacksonville, FL\n904-615-5819\nlawrences@huntington.edu\nlinkedin.com/in/sonya-lt\nhttps://sonyalt.vercel.app'
+Portfolio = 'https://sonyalt.vercel.app'
 QRHeader = 'PORTFOLIO'
 
 ProjectsHeader = 'PROJECTS/PUBLICATIONS'
